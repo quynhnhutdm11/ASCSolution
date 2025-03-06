b7082aa3b7e34652d399b94b844222b8a3700339
quynhnhutdm11 <qnhu11022004@gmail.com>
2025-03-06 14:07:13 +0700

    LAB2

---
+++ ASCWeb/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ASCWeb.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
--- ASCWeb/Program.cs
+++ ASCWeb/Program.cs
@@ -3,26 +3,44 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ASCWeb.Configuration;
 using ASCWeb.Services;
+using ASC.DataAccess.Interfaces;
+using ASC.DataAccess;
+using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>((options) =>
+{
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped<DbContext, ApplicationDbContext>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddOptions();
-builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("AppSetting"));
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("AppSettings"));
 
+builder.Services.AddControllersWithViews();
 
-
-//Add application Services
+//Add application services
 builder.Services.AddTransient<IEmailSender, AuthMessageSender>();
 builder.Services.AddTransient<ISmsSender, AuthMessageSender>();
+builder.Services.AddSingleton<IIdentitySeed, IdentitySeed>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -48,5 +66,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var storageSeed = scope.ServiceProvider.GetRequiredService<IIdentitySeed>();
+    await storageSeed.Seed(
+        scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>(),
+        scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>(),
+        scope.ServiceProvider.GetRequiredService<IOptions<ApplicationSettings>>());
+}
 
 app.Run();
